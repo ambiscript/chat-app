e89a9be225b8c434ac30b19daafba7092a5f6396
Harley Grace <ambiguousscripting@gmail.com>
2020-08-07 03:04:10 -0500

    Fixed server crash due to artifacts from merge conflict

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-
-const app = express();
-
-=======
 const mongoose = require('mongoose');
 const auth = require('./secret.json')["mongodb-auth"];
 
@@ -24,7 +19,6 @@ mongoose.connect(
   console.error(err);
 });
 
->>>>>>> module4-mongodb
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -42,31 +36,6 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-<<<<<<< HEAD
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fj9as8dfuag9adf',
-      title: 'First server-side post',
-      content: 'This comes from the server!'
-    },
-    {
-      id: 'agj9af8gsa9f8g4',
-      title: 'Second server-side post',
-      content: 'This also comes from the server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-=======
   const post = new Post({
     title: req.body.title,
     content: req.body.content
@@ -92,7 +61,6 @@ app.get('/api/posts', (req, res, next) => {
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(() => {
     res.status(200).json({ message: "Post deleted." });
->>>>>>> module4-mongodb
   });
 });
 
